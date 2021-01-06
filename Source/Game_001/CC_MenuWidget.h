2df585b6a5e03f83116110a37aef6d445060438b
Anna Swiatkowska <a.swiatkowska177@gmail.com>
2021-01-06 20:14:35 +0100

    New c++ class

---
+++ Source/Game_001/CC_MenuWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CC_MenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_001_API UCC_MenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
